b1204fd0c9c63ff365e640bd186793985dc5fc8c
AaronVelasco93 <aaronvelasco1993@outlook.com>
2025-02-17 09:31:16 -0600

    Incio de clase del 17022025 v.01 ejemplo de uso de git

---
+++ 17022025/app.js
@@ -0,0 +1,25 @@
+/*
+let nombre  = "Aaron";
+    nombre = "Mario";
+var edad = 5;
+var edad ="ocho";
+    edad = "20";
+const puerto = 3306;
+    puerto = 3308; 
+*/
+
+console.log("Mi nombre es: ");
+    let nombre = "Aaron";
+console.log(nombre);
+
+console.log("Edad: ")
+    let edad=31;
+console.log(edad);
+
+var Aaron ="VEAA911205hflrg02";
+var Aarón ="UNAM";
+
+
+
+
+
---
+++ 17022025/app2.js
@@ -0,0 +1,7 @@
+// Concatenacion
+let uno = "Hola";
+let dos = " mundo";
+var unir = uno + dos;
+console.log(unir);
+ 
+ 
